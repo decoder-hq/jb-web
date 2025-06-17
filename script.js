f1d86ea8547d8117f484d28e745d34600da56c04
Raman Preet Singh <ramanjrpreetsingh9889@gmail.com>
2025-06-17 21:54:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,361 @@
+const body = document.querySelector('.content');
+const loader = document.getElementById('loader')
+const header = document.querySelector('.header');
+const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector('.hamburger');
+const home = document.querySelector(".home")
+const about = document.querySelector(".about")
+const parent = document.querySelector('.parent');
+const parentHeading = document.querySelector('.aboutHeading')
+const subHeading = document.querySelector('#sub-heading')
+const catalogue = document.querySelector(".catalogue")
+const contact = document.querySelector(".contact")
+
+var isActive = false
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    gsap.from('.about-heading', {
+        y: 100,
+        rotateX: -90,
+        skewX: 10,
+        opacity: 0,
+        duration: 1.2,
+        delay: 0.2,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.about-heading',
+            start: "top 90%",
+            end: "bottom 90%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.sub-heading', {
+        y: 100,
+        rotateX: -90,
+        skewX: 10,
+        opacity: 0,
+        duration: 1.2,
+        delay: 0.2,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.sub-heading',
+            start: "top 90%",
+            end: "bottom 95%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.para-1', {
+        y: 100,
+        rotateX: -90,
+        skewX: 10,
+        opacity: 0,
+        duration: 1,
+        delay: 0.3,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.para-1',
+            start: "top 90%",
+            end: "bottom 95%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.para-2', {
+        y: 100,
+        rotateX: -90,
+        skewX: 10,
+        opacity: 0,
+        duration: 1,
+        delay: 0.4,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.para-2',
+            start: "top 90%",
+            end: "bottom 95%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.para-3', {
+        y: 100,
+        rotateX: -90,
+        skewX: 10,
+        opacity: 0,
+        duration: 1,
+        delay: 0.5,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.para-3',
+            start: "top 95%",
+            end: "bottom 95%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.image', {
+        x: 100,
+        opacity: 0,
+        duration: 1,
+        delay: 0.3,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.para-1',
+            start: "top 90%",
+            end: "top 95%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.cat-heading', {
+        y: 100,
+        rotateX: -90,
+        skewX: 10,
+        opacity: 0,
+        duration: 1.2,
+        delay: 0.2,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.cat-heading',
+            start: "top 90%",
+            end: "bottom 90%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.cat-description', {
+        y: 100,
+        rotateX: -90,
+        skewX: 10,
+        opacity: 0,
+        duration: 1.2,
+        delay: 0.4,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.cat-description',
+            start: "top 90%",
+            end: "bottom 95%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+    gsap.from('.slider', {
+        y: 100,
+        rotateX: -45,
+        opacity: 0,
+        duration: 1.2,
+        delay: 1,
+        ease: "circ.out",
+        scrollTrigger: {
+            trigger: '.slider',
+            start: "top 90%",
+            end: "top 98%",
+            toggleActions: "play none reverse none",
+        }
+    })
+
+});
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('active', window.scrollY > 0);
+})
+
+hamburger.addEventListener('click', function () {
+    isActive = !isActive
+    hamburger.classList.toggle('active')
+    header.classList.toggle('expand');
+})
+
+window.addEventListener("load", () => {
+    var caption1 = document.querySelector('.caption-1');
+    caption1.innerHTML = caption1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({ loop: false })
+        .add({
+            targets: '.caption-1 .letter',
+            translateX: [40, 0],
+            translateZ: 0,
+            opacity: [0, 1],
+            easing: "easeOutExpo",
+            duration: 2500,
+            delay: (el, i) => 500 + 60 * i
+        })
+    setTimeout(() => {
+        caption1.innerHTML = "Get water<span class='word'>clean</span> <span class='word'>fresh</span> <span class='word'>pure</span>"
+        caption1.classList.add('spacing')
+    }, 3000)
+})
+
+var caption2 = document.querySelector('.caption-2');
+caption2.innerHTML = caption2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.caption-2 .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 2500,
+        delay: (el, i) => 500 + 60 * i
+    })
+
+var brandName = document.querySelector('.brand-name');
+brandName.innerHTML = brandName.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.brand-name .letter',
+        translateX: [-40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 2500,
+        delay: (el, i) => 500 + 60 * i
+    })
+
+function homeSection() {
+    hamburger.classList.remove('active')
+    header.classList.remove('expand');
+    home.scrollIntoView()
+}
+
+function aboutSection() {
+    hamburger.classList.remove('active')
+    header.classList.remove('expand');
+    about.scrollIntoView()
+}
+
+function catalogueSection() {
+    hamburger.classList.remove('active')
+    header.classList.remove('expand');
+    catalogue.scrollIntoView()
+}
+
+const items = document.querySelectorAll('.slider .item');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let active = 3
+
+function loadShow() {
+    let display = 0
+    items[active].style.transform = `none`;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
+    for (var i = active + 1; i < items.length; i++) {
+        display++;
+        items[i].style.transform = `translateX(${250 * display}px) scale(${1 - 0.2 * display}) perspective(16px) rotateY(1deg)`;
+        items[i].style.filter = 'blur(1px)';
+        items[i].style.opacity = display > 1 ? 0 : 0.6;
+    }
+
+    display = 0
+    for (var i = active - 1; i >= 0; i--) {
+        display++;
+        items[i].style.transform = `translateX(${-250 * display}px) scale(${1 - 0.2 * display}) perspective(16px) rotateY(-1deg)`;
+        items[i].style.filter = 'blur(1px)';
+        items[i].style.opacity = display > 1 ? 0 : 0.6;
+    }
+
+    if (active < 1) {
+        prevBtn.disabled = true
+    }
+    if (active >= 1) {
+        prevBtn.disabled = false
+    }
+    if (active > 4) {
+        nextBtn.disabled = true
+    }
+    if (active <= 4) {
+        nextBtn.disabled = false
+    }
+
+    if (active < 1) {
+        items[0].classList.remove('left')
+        items[0].classList.add('center')
+        items[1].classList.remove('center')
+        items[1].classList.add('right')
+        items[2].classList.remove('right')
+    }
+
+    // if (active == 1) {
+    //     // console.log(active)
+    //     items[0].classList.remove('center')
+    //     items[0].classList.add('left')
+    //     items[1].classList.remove('right')
+    //     items[1].classList.remove('left')
+    //     items[1].classList.add('center')
+    //     items[2].classList.remove('center')
+    //     items[2].classList.add('right')
+    //     items[3].classList.remove('right')
+    // }
+
+    // if (active == 2) {
+    //     // console.log(active)
+    //     items[0].classList.remove('left')
+    //     items[1].classList.remove('center')
+    //     items[1].classList.add('left')
+    //     items[2].classList.remove('right')
+    //     items[2].classList.remove('left')
+    //     items[2].classList.add('center')
+    //     items[3].classList.remove('center')
+    //     items[3].classList.add('right')
+    //     items[4].classList.remove('right')
+    // }
+
+    // if (active == 3) {
+    //     // console.log(active)
+    //     items[1].classList.remove('left')
+    //     items[2].classList.remove('left')
+    //     items[2].classList.remove('center')
+    //     items[2].classList.add('left')
+    //     items[3].classList.remove('right')
+    //     items[3].classList.remove('left')
+    //     items[3].classList.add('center')
+    //     items[4].classList.remove('center')
+    //     items[4].classList.add('right')
+    //     items[5].classList.remove('center')
+    //     items[5].classList.remove('right')
+    // }
+    // if (active == 4) {
+    //     // console.log(active)
+    //     items[2].classList.remove('left')
+    //     items[3].classList.remove('center')
+    //     items[3].classList.add('left')
+    //     items[4].classList.remove('right')
+    //     items[4].classList.remove('left')
+    //     items[4].classList.add('center')
+    //     items[5].classList.remove('center')
+    //     items[5].classList.add('right')
+    // }
+
+    // if (active == 5) {
+    //     // console.log(active)
+    //     items[3].classList.remove('left')
+    //     items[4].classList.remove('center')
+    //     items[4].classList.add('left')
+    //     items[5].classList.remove('right')
+    //     items[5].classList.add('center')
+    // }
+}
+
+loadShow()
+
+function prev() {
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow()
+}
+
+function next() {
+    active = active + 1 < items.length ? active + 1 : active;
+    loadShow();
+}
+
+
+
+
