e653953dbd88642a8ee4210214222709a44b017f
Raman Preet Singh <ramanjrpreetsingh9889@gmail.com>
2025-06-27 21:37:51 +0530

    swiper changed

--- script.js
+++ script.js
@@ -295,7 +295,7 @@ var swiper = new Swiper(".swiper", {
     initialSlide: 3,
     slidesPerView: 'auto',
     preventClicks: true,
-    speed: 800,
+    speed: 400,
     spaceBetween: 0,
     coverflowEffect: {
         rotate: 40,
